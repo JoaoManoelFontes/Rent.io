84d8fc2cf7e6cc16e9b244aa331bbefb05482138
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-05-07 17:23:25 -0300

    rename customers app for core

--- rent_io/settings.py
+++ rent_io/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "customers",
+    "core",
     "properties",
     "cpf_field",
     "phonenumber_field",
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "customers.Customer"
+AUTH_USER_MODEL = "core.Customer"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
