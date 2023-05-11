d5bfcabf8453724ee64d8c2e6c9c4faf37bc4214
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-05-11 16:12:36 -0300

    add properties models

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import House, Building, Apartment, Payment, Media
 
 # Register your models here.
+admin.site.register(House)
+admin.site.register(Building)
+admin.site.register(Apartment)
+admin.site.register(Payment)
+admin.site.register(Media)
