52d9372f083d6a1cd4ade4616ac64aff541f6b98
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-05-30 16:33:47 -0300

    add static files and import bootstrap

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 # []home -> listagem de imóveis; acesso à página do usuário; filtragem de imóveis
 # []customer -> página do usuário; listagem de imóveis do usuário; criação de imóveis
+
+
+def home(request):
+    return render(request, 'core/index.html')
