bb522bd83e0d5b8b0dced6dd7f5f33938afb908e
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-07-12 08:48:42 -0300

    add bith_date on user model

--- core/models.py
+++ core/models.py
@@ -9,6 +9,7 @@ class Customer(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_number = PhoneNumberField()
     full_name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.username
