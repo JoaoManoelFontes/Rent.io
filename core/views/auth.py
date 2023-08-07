0f0faee8734caeddf7d21a0dcbe48a49627aa34e
JoaoManoelFontes <jmanufontesc@gmail.com>
2023-08-07 09:11:51 -0300

    add error handling on sign_up template

--- core/views/auth.py
+++ core/views/auth.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from ..models import Customer 
+from ..models import Customer
 from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
