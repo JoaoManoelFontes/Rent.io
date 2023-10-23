54bce55e08a0fbf05abbeac35f2e4e0edf37e472
Cauã Alexandre <cauasouza112@gmail.com>
2023-10-23 11:15:41 -0300

    Adição_de_About_us_e_bugs_resolvidos

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views.home import home, detail_building, detail_house
+from .views.home import home, detail_building, detail_house, all_houses, all_buildings, about_us
 from .views.auth import sign_in, sign_out, sign_up
 
 urlpatterns = [
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', sign_in, name='sign_in'),
     path('signup/', sign_up, name='sign_up'),
     path('logout/', sign_out, name='sign_out'),
+    path('all_houses/', all_houses, name='all_houses'),
+    path('all_buildings/', all_buildings, name='all_buildings'),
+    path('about_us/', about_us, name='about_us'),
 ]
--- core/views/home.py
+++ core/views/home.py
@@ -7,6 +7,21 @@ def home(request, pk=id):
     return render(request, 'core/home.html', context=home_use_case())
 
 
+def about_us(request):
+    '''About us view.'''
+    return render(request, 'core/about_us.html')
+
+
+def all_houses(request):
+    '''Houses listing view.'''
+    return render(request, 'core/all_houses.html', context=home_use_case())
+
+
+def all_buildings(request):
+    '''Buildings listing view.'''
+    return render(request, 'core/all_buildings.html', context=home_use_case())
+
+
 def detail_building(request, pk=id):
     '''Building detail view.'''
     return render(request, 'core/detail_building.html', context=detail_building_use_case(pk))
