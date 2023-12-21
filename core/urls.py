00420011d456d6f44e45ff9daa8b55204d7906d1
Cauã Alexandre <cauasouza112@gmail.com>
2023-12-21 17:28:25 -0300

    profile_update

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.home import home, detail_building, detail_house, all_houses, all_buildings, about_us
-from .views.auth import sign_in, sign_out, sign_up
+from .views.auth import sign_in, sign_out, sign_up, edit_profile
 
 urlpatterns = [
     path('', home, name='home'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', sign_in, name='sign_in'),
     path('signup/', sign_up, name='sign_up'),
     path('logout/', sign_out, name='sign_out'),
+    path('profile/', edit_profile, name='edit_profile'),
     path('houses/', all_houses, name='all_houses'),
     path('buildings/', all_buildings, name='all_buildings'),
     path('about_us/', about_us, name='about_us'),
